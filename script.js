c1e7bcf223331ff2d4fb696180567c0115b6db8f
eivind-glodedata <eivind.glodedata@gmail.com>
2025-04-29 13:26:22 +0200

    eksperimentering

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-console.log('Hello!');
+console.log(document.querySelector(".hamburgermeny"));
+console.log(document.querySelector("main").innerHTML);
+
+const hamburgerelementet = document.querySelector(".hamburgermeny");
+
+function changeElementStyle(elementId, newColor) {
+    // Get the element by its ID
+
+    let element = document.getElementById(elementId);
+    console.log(element);
+    
+    // Check if the element exists
+    if (element) {
+        // Change the background color of the element
+        element.style.backgroundColor = newColor;
+    } else {
+        console.log("Element not found");
+    }
+}
+
+// Example usage: Change the background color of the element
+changeElementStyle('header', 'brown');
+//changeElementStyle('body', 'pink');
+
+function varsling(param1, param2) {
+    alert(param1 + ' ' + param2);
+}
+
+//myFunction('parameter1', 'parameter2')
+
+//Alternatively, you can use an event listener for a more modern approach:
+
+document.getElementById("myButton").addEventListener("click", function() {
+    alert("Button was clicked!");
+});
+
+//assign a function to the onclick event of the button using JavaScript
+
+document.getElementById("myButton").onclick = function() {
+    alert("Button was clicked!");
+};
+
+
+
+function test() {alert("funksjonen funker!")
+};
+
+
